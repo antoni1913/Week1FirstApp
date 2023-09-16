36150d90d75784c21a277ddf5d6ddc71bfefe806
antoni1913 <antonisariev13@gmail.com>
2023-09-17 01:44:55 +0300

    add my first app

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string myName = "Antoni";
+string myLastName = "Sariev";
+
+Console.WriteLine($"Hello, {myName} {myLastName} !");
+                
